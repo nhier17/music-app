788a0760f3a058357cae34319e91a94866ee3187
Abraham <abrahamnhier97@gmail.com>
2024-02-03 15:30:58 +0300

    changed to normal css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState,useRef} from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import chillhop from "./Music"
-import "./styles/app.scss";
+import "./styles/app.css";
 import Library from "./components/Library";
 import Nav from "./components/Nav";
  
